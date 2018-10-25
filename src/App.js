a6aea0fb6c285e2324ea67e4aef53fe42505f66d
Vina Compson <vv.compson@icloud.com>
2018-10-25 11:05:36 +1100

    initial commit with array robot list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './CardList';
+import { robots } from './robots';
+import Searchbox from './Searchbox';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+        })
+        return(
+            <div className='tc'>
+                <h1 className = 'f1'>RoboFriends</h1>
+                <Searchbox searchChange={this.onSearchChange}/>
+                <CardList robots={ filteredRobots }/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className='bg-light-pink dib br3 pa3 ma2 grow bw2 shaddow-5'>
+            < img alt = "robots"
+            src = {
+                `https://robohash.org/${id}?100x100?`
+            }
+            />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
