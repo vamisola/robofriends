fc243ea807177f96a87013cc13a8ea60f953e701
Vina Compson <vv.compson@icloud.com>
2018-11-12 11:00:39 +1100

    modify Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, email, id}) => {
     return (
         <div className='bg-light-pink dib br3 pa3 ma2 grow bw2 shaddow-5'>
-            < img alt = "robots"
+            <img alt = "robots"
             src = {
                 `https://robohash.org/${id}?100x100?`
             }
